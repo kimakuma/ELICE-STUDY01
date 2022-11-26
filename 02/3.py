fb3c9d0b6eca183c364ddee47c9f4e6a9e8d8de5
kimakuma <76460405+kimakuma@users.noreply.github.com>
2022-11-27 05:47:03 +0900

    Delete 02 directory

--- 02/3.py
+++
@@ -1,6 +0,0 @@
-line_num = int(input())
-
-for i in range(line_num): 
-  str = input()
-  
-  print(str[0].upper() + str[1:].lower())
--- 02/4.py
+++
@@ -1,7 +0,0 @@
-word_count = list(map(int, input().split()))
-
-word_A = input()
-word_B = input()
-
-if word_A in word_B: print(1)
-else: print(0)
